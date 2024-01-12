ceadfd53cdacac88e41b3308bad1c56e70fac622
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2024-01-13 00:11:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+config = {
+    "google_api_key" :"AIzaSyAHZibHAVeiNFLg0lxNpaK5BuOB7Zzi6zM",
+    "youtube_playlist_id": "PLr3-bEi1N4khKZm2u6Q8pOLO-WNZrKDHn"
+}
+
+
+config = {
+    "google_api_key": "AIzaSyAHZibHAVeiNFLg0lxNpaK5BuOB7Zzi6zM",
+    "youtube_playlist_id": "PLr3-bEi1N4khKZm2u6Q8pOLO-WNZrKDHn",
+    "kafka": {
+        "bootstrap.servers": "pkc-4r087.us-west2.gcp.confluent.cloud:9092",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "32SOA37EFE2K6TQH",
+        "sasl.password": "Zh3hQADxRZvBxif6VLRH5og9NZl4kNZo16XGwb8WdMPBErU0mtR8tv3vaaysVI5M",
+    },
+    "schema_registry": {
+        "url": "https://psrc-q8qx7.us-central1.gcp.confluent.cloud",
+        "basic.auth.user.info": "FOAYNWT2SADHLZZ5:Hkd44r6z4Atpb/Zmnn2AfaSSN7ojJNWUhOaS8E3y2JliHSeOYSrpPBFM42cvz/8Z",
+    }
+}
